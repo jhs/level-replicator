eaed0aaca5fea2174e2cbbf571e80fb184c3c7dc
Jason Smith (work) <jason.h.smith@gmail.com>
2014-05-18 15:44:35 +0000

    XXX Sun May 18 15:44:35 UTC 2014

--- replicator.js
+++ replicator.js
@@ -6,8 +6,11 @@ module.exports = replicator
 var hooks = require('level-hooks')
 var sublevel = require('level-sublevel')
 var SubDB = require('level-sublevel/sub.js')
+var stringRange = require('string-range')
 
-function replicator(db) {
+var VERSION = require('./package.json').version
+
+function replicator(db, callback) {
   if (!db || !db.options || db.options.keyEncoding != 'utf8')
     throw new Error('Only utf8 key encoding is supported')
   if (!db || !db.options || db.options.valueEncoding != 'json')
@@ -15,8 +18,51 @@ function replicator(db) {
   if (db instanceof SubDB)
     throw new Error('Replicator must be installed on a top-level database') // TODO: Is this necessary?
 
-  db = sublevel(db)
-  var replicator = db.sublevel('_replicator', {keyEncoding:'json'})
+  if (db.replicator)
+    return db
 
-  return replicator
+  db = sublevel(db)
+  db.replicator = db.sublevel('_replicator', {valueEncoding:'json'})
+
+  db.replicator.version = VERSION
+  db.replicator.put('version', VERSION, function(er) {
+    db.replicator.emit('log', 'Initialized with error=%j', !!er)
+    if (callback)
+      callback(er)
+  })
+
+  var not_my_key = anti_checker(db.replicator)
+  db.hooks.pre(not_my_key, function(change, add, batch) {
+    log_change(db, change, add, batch)
+  })
+
+  db.replicator.on('log', console.log)
+  return db
+}
+
+function log_change(db, change, add, batch) {
+  console.log('pre-change', change)
+  var replicator = db.replicator
+
+  var last = batch[batch.length - 1]
+  if (last._replicator)
+    return replicator.emit('log', 'Change log already added: %j', last)
+
+  batch = JSON.parse(JSON.stringify(batch))
+  var log = {type:'put', key:1, _replicator:true, value:JSON.parse(JSON.stringify(batch))}
+  console.log('add to changes: %j', log)
+  //add(log, changes)
+}
+
+// Return a checker for all keys NOT within this sublevel.
+function anti_checker(db) {
+  var min = db.prefix()
+  var max = min + db._options.sep
+
+  var checker = stringRange.checker({'min':min, 'max':max})
+  function no_checker(key) {
+    return ! checker(key)
+  }
+
+  return no_checker
 }
--- test/replicator.js
+++ test/replicator.js
@@ -4,10 +4,15 @@ var memdown = require('memdown')
 var levelup = require('levelup')
 var sublevel = require('level-sublevel')
 var replicator = require('../replicator')
+var PACKAGE = require('../package.json')
 
 test('Initialization', function(t) {
   var db, rep, er
 
+  try { rep = replicator() } catch (e) { er = e }
+  t.ok(er, 'Exception for no db provided')
+
+  er = null
   db = levelup('db', {db:memdown, keyEncoding:'binary'})
   try { rep = replicator(db) } catch (e) { er = e }
   t.ok(er, 'Exception for non-utf8 keyEncoding')
@@ -18,18 +23,61 @@ test('Initialization', function(t) {
   t.ok(er, 'Exception for non-JSON valueEncoding')
 
   er = null
-  db = levelup('db', {db:memdown, keyEncoding:'utf8', valueEncoding:'json'})
-  try { rep = replicator(db) } catch (e) { er = e }
-  t.same(er, null, 'No error for keyEncoding:utf8 and valueEncoding:binary')
-
-  t.ok(rep, 'Replicator db was returned')
-
-  er = null
   db = sublevel(db)
   var someSub = db.sublevel('someSub')
   db = levelup('db', {db:memdown, keyEncoding:'utf8', valueEncoding:'json'})
   try { rep = replicator(someSub) } catch (e) { er = e }
   t.ok(er, 'Exception for replicator on a sublevel')
 
+  er = null
+  db = levelup('db', {db:memdown, keyEncoding:'utf8', valueEncoding:'json'})
+  try { rep = replicator(db) } catch (e) { er = e }
+  t.same(er && er.message, null, 'No error for keyEncoding:utf8 and valueEncoding:binary')
+  t.ok(rep, 'Replicating db was returned')
+
   t.end()
 })
+
+test('Hooks batching', function(t) {
+  var db = levelup('db', {db:memdown, valueEncoding:'json'})
+  var old = db
+  db = replicator(db, on_init)
+
+  function on_init(er) {
+    t.ok(!er, 'No problem initializing')
+    t.ok(db, 'Returned a replicating db')
+    t.ok(db.replicator, 'Replicating db has a reference to its replicator subDB')
+
+    t.equal(db.replicator.version, PACKAGE.version, 'Replicator set its version')
+    db.replicator.get('version', function(er, ver) {
+      if(er) throw er
+      t.equal(ver, PACKAGE.version, 'Replicator version was stored')
+      init_data()
+    })
+  }
+
+  function init_data() {
+    db.put('jason', 10, function(er) {
+      t.ok(!er, 'No problem putting value: jason')
+
+    db.put('issaree', 10, function(er) {
+      t.ok(!er, 'No problem putting value: issaree')
+
+    db.batch([ {type:'put', key:'jason', value:5}
+             , {type:'put', key:'issaree', value:15}
+             ] , function (er) {
+      t.ok(!er, 'No problem running a batch')
+      dir(old)
+
+    t.end() }) }) })
+  }
+})
+
+function dir(db) {
+  console.log('')
+  db.createReadStream({end:null})
+  .on('data', function(data) {
+    var key = data.key.replace(/\xff/g, '/')
+    console.log('%s = %j', key, data.value)
+  })
+}
